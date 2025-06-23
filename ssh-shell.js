ab858b06cb3ff5ab53b3054c8a7209c981a826b5
caltuntas <cihat.altuntas@gmail.com>
2025-06-24 01:04:15 +0300

    add ssh shell based ls and produce wireshark key log file from captured output

---
+++ ssh-shell.js
@@ -0,0 +1,73 @@
+const { readFileSync } = require('fs');
+
+const { Client } = require('ssh2');
+
+const algorithms = {
+	kex: [
+	  //'ecdh-sha2-nistp256',
+	  //'ecdh-sha2-nistp384',
+	  //'ecdh-sha2-nistp521',
+	  //'diffie-hellman-group-exchange-sha256',
+	  //'diffie-hellman-group14-sha1',
+	  //'diffie-hellman-group-exchange-sha1',
+	  //'diffie-hellman-group16-sha512',
+	  'diffie-hellman-group1-sha1',
+	],
+	cipher: [
+	  'aes128-ctr',
+	  //'aes192-ctr',
+	  //'aes256-ctr',
+	  //'aes128-gcm',
+	  //'aes128-gcm@openssh.com',
+	  //'aes256-gcm',
+	  //'aes256-gcm@openssh.com',
+	  //'aes256-cbc',
+	  //'aes192-cbc',
+	  //'aes128-cbc',
+	  //'blowfish-cbc',
+	  //'3des-cbc',
+	  //'arcfour256',
+	  //'arcfour128',
+	  //'cast128-cbc',
+	  //'arcfour',
+	],
+	hmac: [
+	  'hmac-sha2-256',
+	  //'hmac-sha2-512',
+	  //'hmac-sha1',
+	  //'hmac-md5',
+	  //'hmac-sha2-256-96',
+	  //'hmac-sha2-512-96',
+	  //'hmac-ripemd160',
+	  //'hmac-sha1-96',
+	  //'hmac-md5-96',
+	],
+	//compress: ['none', 'zlib@openssh.com', 'zlib'],
+	//serverHostKey: ['ssh-dss', 'ssh-rsa', 'rsa-sha2-256', 'rsa-sha2-512', 'ecdsa-sha2-nistp521', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp256', 'ssh-ed25519'],
+  };
+
+
+const conn = new Client();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+  conn.shell((err, stream) => {
+    if (err) throw err;
+    stream.on('close', () => {
+      console.log('Stream :: close');
+      conn.end();
+    }).on('data', (data) => {
+      console.log('OUTPUT: ' + data);
+    });
+    stream.end('cd /tmp && ls -lah\nexit\n');
+  });
+}).connect({
+		host: process.env.TARGET_HOST,
+		port: 22,
+		username: process.env.TARGET_USERNAME,
+		password: process.env.TARGET_PASSWORD,
+		algorithms: algorithms,
+		debug: function(msg) {
+			console.log(msg);
+		},
+});
+
--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 nohup tcpdump --immediate-mode -w ssh.pcap </dev/null &>/dev/null &
 node ssh-ls.js | tee output.log | grep -A1 'config---' | grep -v 'config---' > config.json
+awk '/randomFill/{s=$4" SHARED_SECRET "} /SECRET/{s=s substr($2,9)} END{print s}' output.log > wireshark-key.log
 tcpdumppid=$(pgrep tcpdump)
 kill "$tcpdumppid"
 node ssh-traffic-dump.js
